1964d58f72038e3aa4e6f6a098907aa351a0f935
Bencze Gergo <bencze.gergo7@gmail.com>
2017-06-05 00:46:21 +0200

    All links print from an option web page, and write to links.txt.

--- map.py
+++ map.py
@@ -5,13 +5,11 @@ links= open("links.txt","w")
 url=file.readline()
 file.close()
 
-
 from selenium import webdriver
 driver = webdriver.Edge('C:/Users/bence/Dokumentumok/Map/MicrosoftWebDriver.exe')
 driver.get(url)
 
 ids= driver.find_elements_by_xpath('//*[@href]')
-
 
 for id in ids:
 	print(id.get_attribute('href'))
